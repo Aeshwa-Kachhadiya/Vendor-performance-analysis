fbd4f3dce0ab87ee19bed7ab4ba91de8c9deb832
Aeshwa-Kachhadiya <aeswa0001@gmail.com>
2026-01-02 15:31:16 -0600

    Add module docstring and metadata to __init__.py

--- pipeline/__init__.py
+++ pipeline/__init__.py
@@ -1,1 +1,11 @@
+"""
+Vendor Analytics Pipeline Package
+"""
 
+__version__ = "1.0.0"
+__author__ = "Your Name"
+
+from .pipeline import run_pipeline
+from .watcher import start_watcher
+
+__all__ = ['run_pipeline', 'start_watcher']
